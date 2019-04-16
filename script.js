21b099565c14da1d61dbaf8a7fcc5468ed42d7c8
Tom Watson <tdw3@mit.edu>
2019-04-16 01:28:02 -0400

    new style

---
+++ script.js
@@ -0,0 +1,21 @@
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('June 01, 2019 16:00:00 MST').getTime(),
+    x = setInterval(function() {
+      let now = new Date().getTime(),
+          distance = countDown - now;
+      if (distance < 0) {
+        distance = 0;
+      }
+      document.getElementById('days').innerText=
+        Math.floor(distance / (day));
+      document.getElementById('hours').innerText=
+        Math.floor((distance % (day)) / (hour));
+      document.getElementById('minutes').innerText=
+        Math.floor((distance % (hour)) / (minute));
+      document.getElementById('seconds').innerText=
+        Math.floor((distance % (minute)) / second);
+    }, second)
